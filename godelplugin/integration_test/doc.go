926ff9d9384a655f91ac55f93652c1182185f85c
Nick Miyake <nmiyake@users.noreply.github.com>
2018-04-28 12:29:40 -0700

    Remove gödel plugin (#16)

--- godelplugin/integration_test/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2016 Palantir Technologies Inc. All rights reserved.
-// Use of this source code is governed by the Apache License, Version 2.0
-// that can be found in the LICENSE file.
-
-// Package integration contains the integration tests for distgo.
-package integration
